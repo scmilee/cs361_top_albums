82e088a38c3b55749f964dc49df73d90cc0a31fb
Yong Bakos <ybakos@humanoriented.com>
2018-02-12 15:52:28 -0800

    Stub response workflow.

--- album_app.rb
+++ album_app.rb
@@ -1,7 +1,13 @@
 class AlbumApp
 
   def call(env)
-    [200, {'Content-Type' => 'text/html'}, ['<h1>Top 100 Albums of All Time</h1>']]
+    response_body = "<h1>Top 100 Albums of All Time</h1>"
+    # Read the data from the file.
+    albums = ['fee', 'fi', 'fo', 'funk']
+    # Append it to the response body.
+    response_body << albums.to_s
+    # Send the response
+    [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
   end
 
 end
