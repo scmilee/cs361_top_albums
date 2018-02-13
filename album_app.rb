ec64253bf23e516e6e2f14216df99e5a307c5968
Dylan Drudge <drudged@oregonstate.edu>
2018-02-13 13:58:39 -0800

    Add logic/data serperation

--- album_app.rb
+++ album_app.rb
@@ -5,12 +5,70 @@ class AlbumApp
   def call(env)
 
     req = Rack::Request.new(env)
+    index = req.params["number"] || 0
     path = req.path_info
     #query = req.params["sortBy"]
     highlightz= req.params["highlight"]
 
     albums = []
-    response_body = "
+    response_body = ""
+
+    #call cssGen and openFile to generate css and the entries into albums
+    cssGen(response_body)
+    openFile(albums)
+    #the query/ path handlers works for either one once the formaction is changed for each button
+    if path == '/Alphabetical'
+    #if query == 'alphabet'
+      sortedAl = albums.sort {|a,b| a[0] <=> b[0]}
+      listGenerator(sortedAl, index, response_body)
+
+    elsif path == '/Year'
+    #elsif query == 'year'
+      sortedAlb = albums.sort {|a,b| a[1] <=> b[1]}
+      listGenerator(sortedAlb, index, response_body)
+    else
+      listGenerator(albums, index, response_body)
+    end
+    [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
+  end
+
+  def openFile(albums)
+    #have a each loop to sort through the matrix removing the "" and [] from the output
+    #open the .txt file, read its strings, and add them to the album
+    File.open("top_100_albums.txt").each do |line|
+
+      newline = line.to_s
+      #chop cuts off the ugly \n after every entry
+      1.times do newline.chop! end
+      #splits the line to be able to index the date later for sorting
+      albumSplit = newline.split(', ')
+      #puts the albumsplit into the array to be turned into html later
+      albums << albumSplit
+    end
+  end
+
+  def listGenerator(albums,number,response_body)
+
+      listGen = lambda{|albums,number|
+        index = 0
+        albums.each do |splittedAlbum|
+          index += 1
+          if index.to_s === number.to_s
+            response_body << '<li class="highlighted">'
+          else
+            response_body << "<li>"
+          end
+          splittedAlbum.each do |x|
+            response_body << x + "  "
+          end
+          response_body << "</li>"
+        end
+        response_body << "</ol>"
+      }
+      listGen.call(albums,number)
+  end
+  def cssGen(response_body)
+    response_body << "
     <style>
     h1 {
        background-color: #e74c3c;
@@ -40,66 +98,6 @@ class AlbumApp
     </form>
     <br>
     <ol>"
-#have a each loop to sort through the matrix removing the "" and [] from the output
 
-
-    #open the .txt file, read its strings, and add them to the album
-    openFile(albums)
-
-    #the query/ path handlers works for either one once the formaction is changed for each button
-    #if path == '/highlight'
-
-    if path == '/Alphabetical'
-    #if query == 'alphabet'
-
-      sortedAl = albums.sort {|a,b| a[0] <=> b[0]}
-      index = req.params["number"] || 0
-      listGenerator(sortedAl, index, response_body)
-
-    elsif path == '/Year'
-    #elsif query == 'year'
-      sortedAlb = albums.sort {|a,b| a[1] <=> b[1]}
-      index = req.params["number"] || 0
-      listGenerator(sortedAlb, index, response_body)
-    else
-      index = req.params["number"] || 0
-      listGenerator(albums, index, response_body)
-    end
-    [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
-  end
-
-  def openFile(albums)
-
-    File.open("top_100_albums.txt").each do |line|
-
-      newline = line.to_s
-      #chop cuts off the ugly \n after every entry
-      1.times do newline.chop! end
-      #splits the line to be able to index the date later for sorting
-      albumSplit = newline.split(', ')
-      #puts the albumsplit into the array to be turned into html later
-      albums << albumSplit
-    end
-    def listGenerator(albums,number,response_body)
-
-      listGen = lambda{|albums,number|
-        index = 0
-        albums.each do |splittedAlbum|
-          index += 1
-          if index.to_s === number.to_s
-            response_body << '<li class="highlighted">'
-          else
-            response_body << "<li>"
-          end
-          splittedAlbum.each do |x|
-            response_body << x + "  "
-          end
-          response_body << "</li>"
-        end
-        response_body << "</ol>"
-      }
-      listGen.call(albums,number)
-
-    end
   end
 end
