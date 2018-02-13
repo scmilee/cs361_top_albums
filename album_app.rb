b90536fb7d9b7e5cdc0c812768fa3b3cb4728794
Dylan Drudge <drudged@oregonstate.edu>
2018-02-12 20:05:12 -0800

    Add and Adjust how the lines are being added to the repsonse body
    Added the file function that reads each line as well as adjusted how the album array gets added to the reposnse body to avoid unnecesarry commas and brackets in the html output.

--- album_app.rb
+++ album_app.rb
@@ -1,12 +1,30 @@
+require 'CSV'
 class AlbumApp
 
   def call(env)
-    response_body = "<h1>Top 100 Albums of All Time</h1>"
+    albums = []
+    response_body = "<h1>Top 100 Albums of All Time</h1><br><br><input placeholder='highlight a song number....'></input><button>Submit</button><br><ol>"
+
+    File.open("top_100_albums.txt").each do |line|
+      newline = line.to_s
+      #chop cuts off the ugly \n after every entry
+      1.times do newline.chop! end
+      #splits the line to be able to index the date later for sorting
+      newline.split(',', 2)
+
+      listItem = "<li>" + newline + "</li>"
+      #puts the listItem into the array to be added on later
+      albums << listItem
+    end
     # Read the data from the file.
-    albums = ['fee', 'fi', 'fo', 'funk']
-    # Append it to the response body.
-    response_body << albums.to_s
-    # Send the response
+
+
+
+    #have a each loop to get rid of the commas and brackets of the array
+    albums.each do |listedItem|
+      response_body << listedItem
+    end
+    response_body << "</ol>"
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
   end
 
