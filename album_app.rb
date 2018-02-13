f1a8df5f8b770cd54b295897f26c9567e1001afc
Dylan Drudge <drudged@oregonstate.edu>
2018-02-12 22:36:42 -0800

    Add buttons and actions for them.
    added path handlers as well.

--- album_app.rb
+++ album_app.rb
@@ -1,9 +1,23 @@
-require 'CSV'
+require 'rack'
 class AlbumApp
 
+
   def call(env)
+
+    req = Rack::Request.new(env)
+    path = req.path
+
     albums = []
-    response_body = "<h1>Top 100 Albums of All Time</h1><br><br><input placeholder='highlight a song number....'></input><button>Submit</button><br><ol>"
+    response_body = "<h1>Top 100 Albums of All Time</h1><br><br>
+    <form action = '/highlight'>
+    <input placeholder='highlight a song number....'></input>
+    <button type='submit'>Submit</button>
+    <button type='submit' formaction='/'>Rank</button>
+    <button type='submit' formaction='/Alphabetical'>Alphabetical</button>
+    <button type='submit' formaction='/Year'>Year</button>
+    </form>
+    <br>
+    <ol>"
 
     File.open("top_100_albums.txt").each do |line|
 
@@ -12,11 +26,19 @@ class AlbumApp
       1.times do newline.chop! end
       #splits the line to be able to index the date later for sorting
       albumSplit = newline.split(', ')
-
-
       #puts the albumsplit into the array to be turned into html later
       albums << albumSplit
     end
+    if path == '/Alphabetical'
+      albums.sort_by {|a,b| a[0] <=> b[0]}
+      puts albums
+    elsif path == '/Year'
+      albums.sort {|a,b| a[1] <=> b[1]}
+    end
+
+
+
+
 
     #have a each loop to sort through the matrix removing the "" and [] from the output
     albums.each do |splittedAlbum|
