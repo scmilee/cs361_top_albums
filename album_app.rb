c784045aab2b9475320a9d8a59cf116f4677eb2f
Dylan Drudge <drudged@oregonstate.edu>
2018-02-12 21:36:54 -0800

    Fix how the document was being read and being outputed
    made the lines be split when they come in from the document. And changed the reponse body loop to iterate properly though the matrix.

--- album_app.rb
+++ album_app.rb
@@ -6,25 +6,29 @@ class AlbumApp
     response_body = "<h1>Top 100 Albums of All Time</h1><br><br><input placeholder='highlight a song number....'></input><button>Submit</button><br><ol>"
 
     File.open("top_100_albums.txt").each do |line|
+
       newline = line.to_s
       #chop cuts off the ugly \n after every entry
       1.times do newline.chop! end
       #splits the line to be able to index the date later for sorting
-      newline.split(',', 2)
+      albumSplit = newline.split(', ')
 
-      listItem = "<li>" + newline + "</li>"
-      #puts the listItem into the array to be added on later
-      albums << listItem
+
+      #puts the albumsplit into the array to be turned into html later
+      albums << albumSplit
     end
-    # Read the data from the file.
 
-
-
-    #have a each loop to get rid of the commas and brackets of the array
+    #have a each loop to sort through the matrix removing the "" and [] from the output
     albums.each do |listedItem|
-      response_body << listedItem
+      response_body << "<li>"
+      listedItem.each do |x|
+        response_body << x + "  "
+      end
+      response_body << "</li>"
     end
     response_body << "</ol>"
+
+
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
   end
 
