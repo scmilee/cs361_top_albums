918ef9b340c7a84b113cccc9f792306f85afe1f5
Camron Fritz <camfritz@10-116-51-93.wireless.oregonstate.edu>
2018-02-14 14:33:14 -0800

    Clean up naming conventions

--- album_app.rb
+++ album_app.rb
@@ -1,28 +1,27 @@
 require 'rack'
+
 class AlbumApp
 
   def call(env)
-
     req = Rack::Request.new(env)
-    index = req.params["number"] || 0
+    highlight_index = req.params["number"] || 0
     #path = req.path_info
-    highlightz= req.params["highlight"]
-    storedQuery = req.params["storedSort"]
-    query =  req.params["sortBy"] || storedQuery
+    stored_query = req.params["storedSort"]
+    query =  req.params["sortBy"] || stored_query
 
     albums = []
     response_body = ""
 
     #call cssGen and openFile to generate css and the entries into albums
-    cssHtmlGen(response_body,query,storedQuery)
-    readFile(albums)
+    css_html_gen(response_body,query,stored_query)
+    read_file(albums)
     #the query/ path handlers works for either one once the formaction is changed for each button
-    pathHandler(albums,index,response_body,query)
+    path_handler(albums,highlight_index,response_body,query)
 
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
   end
 
-  def readFile(albums)
+  def read_file(albums)
     #have a each loop to sort through the matrix removing the "" and [] from the output
     #open the .txt file, read its strings, and add them to the album
     File.open("top_100_albums.txt").each do |line|
@@ -30,22 +29,22 @@ class AlbumApp
       #chop cuts off the ugly \n after every entry
       1.times do newline.chop! end
       #splits the line to be able to index the date later for sorting
-      albumSplit = newline.split(', ')
-      #puts the albumsplit into the array to be turned into html later
-      albums << albumSplit
+      album_split = newline.split(', ')
+      #puts the album_split into the array to be turned into html later
+      albums << album_split
     end
   end
 
-  def listGenerator(albums,number,response_body)
-    index = 0
-    albums.each do |splittedAlbum|
-      index += 1
-      if index.to_s === number.to_s
+  def list_generator(albums,number,response_body)
+    highlight_index = 0
+    albums.each do |splitted_album|
+      highlight_index += 1
+      if highlight_index.to_s === number.to_s
         response_body << '<li class="highlighted">'
       else
         response_body << "<li>"
       end
-      splittedAlbum.each do |x|
+      splitted_album.each do |x|
         response_body << x + "  "
       end
       response_body << "</li>"
@@ -53,49 +52,49 @@ class AlbumApp
     response_body << "</ol>"
   end
 
-  def pathHandler(albums, index, response_body,query)
+  def path_handler(albums, highlight_index, response_body,query)
     #if path == '/Alphabetical'
     if query == 'alphabet'
       albums = albums.sort {|a,b| a[0] <=> b[0]}
     #elsif path == '/Year'
-    elsif query == 'year'
-      albums = albums.sort {|a,b| a[1] <=> b[1]}
-    end
-      listGenerator(albums, index, response_body)
+  elsif query == 'year'
+    albums = albums.sort {|a,b| a[1] <=> b[1]}
   end
+  list_generator(albums, highlight_index, response_body)
+end
 
-  def cssHtmlGen(response_body,query,storedSort)
-    response_body << "
-    <style>
-    h1 {
-       background-color: #e74c3c;
-       color: color: #ecf0f1;s
-    }
-    form {
-      width: 50%;
-      float: left;
-    }
-    ol {
-      width: 35%;
-    }
-    .highlighted {
-      background-color: tomato;
-      color: black;
-    }
-    .centered {
-      float: left;
-      margin: auto;
-      width: 50%;
-      padding: 10px;
-      margin-bottom: 10px;
-    }
-    button {
-      margin-bottom: 10px;
-    }
-    </style>
-    <h1>Top 100 Albums of All Time</h1><br><br>
-    <form action= '/albums'>
-    <input type='hidden' name='storedSort' value='"
+def css_html_gen(response_body,query,storedSort)
+  response_body << "
+  <style>
+  h1 {
+   background-color: #e74c3c;
+   color: color: #ecf0f1;s
+ }
+ form {
+  width: 50%;
+  float: left;
+}
+ol {
+  width: 35%;
+}
+.highlighted {
+  background-color: tomato;
+  color: black;
+}
+.centered {
+  float: left;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+button {
+  margin-bottom: 10px;
+}
+</style>
+<h1>Top 100 Albums of All Time</h1><br><br>
+<form action= '/albums'>
+<input type='hidden' name='storedSort' value='"
 
     #carry over hidden value from the req
     response_body << (query || "")
