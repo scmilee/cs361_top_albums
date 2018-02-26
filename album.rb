5c6b0ed2458403be69d0d1a97468a53f22e67711
scmilee <drudged@oregonstate.edu>
2018-02-26 15:52:35 -0800

    Reorder attr_accessor declaration.

--- album.rb
+++ album.rb
@@ -1,10 +1,13 @@
 class Album
-	def initialize(rank, year, title)
-		@rank = rank
-		@year = year
-		@title = title
-	end
-	attr_accessor :rank
-	attr_accessor :year
-	attr_accessor :title
+
+  attr_accessor :rank
+  attr_accessor :year
+  attr_accessor :title
+
+  def initialize(rank, year, title)
+    @rank = rank
+    @year = year
+    @title = title
+  end
+
 end
