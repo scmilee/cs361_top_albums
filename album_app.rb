a58ff1cf8109cc96657a4936d201e99351c0f9d6
scmilee <drudged@oregonstate.edu>
2018-02-26 15:53:01 -0800

    Use sinatra erb template functionality.

--- album_app.rb
+++ album_app.rb
@@ -7,35 +7,37 @@ require_relative 'albumlist'
 require_relative 'htmlgen'
 
 class AlbumApp < Sinatra::Base
- albums = AlbumList.new
- helpers do
-  def response_gen(highlight, albumz)
-    response = albumz.htmlgenerator(highlight)
-    [200, {'Content-Type' => 'text/html'}, [response.to_s]]
+  albums = AlbumList.new
+  helpers do
+    def response_gen(highlight, albumz)
+      response = albumz.htmlgenerator(highlight)
+      [200, {'Content-Type' => 'text/html'}, [response.to_s]]
+    end
   end
-end
-before do
-  @highlight_index = params["number"] || 0
 
-end
+  before do
+    @highlight_index = params["number"] || 0
+  end
 
-get '/' do
-  response_gen(@highlight_index, albums)
-end
+  get '/' do
+    @highlight_index = params["number"] || 0
+    @albums = AlbumList.new.albums
+    erb :index
+  end
 
-get '/rank' do
-  albums.sort('rank')
-  response_gen(@highlight_index, albums)
-end
+  get '/rank' do
+    albums.sort('rank')
+    response_gen(@highlight_index, albums)
+  end
 
-get '/alphabet' do
-  albums.sort('title')
-  response_gen(@highlight_index, albums)
-end
-get '/year' do
-  albums.sort('year')
-  response_gen(@highlight_index, albums)
-end
+  get '/alphabet' do
+    albums.sort('title')
+    response_gen(@highlight_index, albums)
+  end
+  get '/year' do
+    albums.sort('year')
+    response_gen(@highlight_index, albums)
+  end
 
 end
 
