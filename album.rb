5cad3f8b79f903e95e35bcfbec5b1c2de67d6a8c
Dylan Drudge <drudged@oregonstate.edu>
2018-02-26 17:25:50 -0800

    Add highlight attribute to album class

--- album.rb
+++ album.rb
@@ -3,11 +3,13 @@ class Album
   attr_accessor :rank
   attr_accessor :year
   attr_accessor :title
+  attr_accessor :highlight
 
-  def initialize(rank, year, title)
+  def initialize(rank, year, title, highlight)
     @rank = rank
     @year = year
     @title = title
+    @highlight = highlight
   end
-
+  
 end
